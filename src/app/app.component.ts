ec0cdfe77699bda0f55907e449c890768b97daf8
Jon Barson <jbarson@gmail.com>
2016-08-01 16:24:10 -0400

    integrated attitude component with app.comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { AttitudeComponent } from './attitude/attitude.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [AttitudeComponent]
 })
 export class AppComponent {
-  foo="thing"
+  foo = 'thing';
 }
