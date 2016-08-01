37645546510a0321e7cbe257ff4096adffceea16
Jon Barson <jbarson@gmail.com>
2016-08-01 16:16:23 -0400

    added Attitude Component framework

---
+++ src/app/attitude/attitude.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-attitude',
+  templateUrl: 'attitude.component.html',
+  styleUrls: ['attitude.component.css']
+})
+export class AttitudeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
